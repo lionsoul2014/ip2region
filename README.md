a7021dc12a230e24a31af550d5a88a348a779362
狮子的魂 <chenxin619315@gmail.com>
2015-11-02 01:12:43 +0800

    yat, hello ip2region

---
+++ README.md
@@ -0,0 +1,69 @@
+ip2region - ip到地区的映射库，妈妈再也不同担心我的ip定位。
+
+**1. 99.9%准确率，不定时更新：**
+
+数据聚合了一些知名ip到地名查询提供商的数据，这些是他们官方的的准确率，经测试着实比纯真啥的准确多了。<br />
+每次聚合一下数据需要1-2天，会不定时更新。
+
+**2. 标准化的数据格式：**
+
+每条ip数据段都固定了格式：_城市Id|国家|区域|省份|城市|ISP_<br />
+只有中国的数据精确到了城市，其他国家只能定位到国家，后前的选项全部是0，已经包含了全部你能查到的大大小小的国家。<br />
+（请忽略前面的城市Id，个人项目需求）
+
+**3. 体积小：**
+
+生成的数据库文件ip2region.db只有3.5M
+
+**4. 多查询客户端的支持，0.x/0.xx毫秒级别的查询**
+
+已经集成的客户端有：java, php, c。
+<pre>
+提供了两种查询算法，响应时间如下：
+客户端/binary算法/b-tree算法：
+java/2.x毫秒/1.x毫秒 (使用RandomAccessFile)
+php/0.x毫秒/0.1x毫秒
+c/0.0x毫秒/0.0x毫秒(b-tree算法基本稳定在0.02x毫秒级别)
+</pre>
+
+任何客户端b-tree都比binary算法快
+
+**5. 测试程序：**
+java: 
+<pre>
+	cd binding/java
+	ant all
+	java -jar ip2region-{version}.jar ./data/ip2region.db
+</pre>
+
+php: 
+<pre>
+    php binding/php/testSearch ./data/ip2region.db
+</pre>
+
+c: 
+<pre>cd binding/c/
+    gcc -g -O2 testSearch.c ip2region.c
+    ./a.out ../../data/ip2region.db
+</pre>
+
+均会看到如下界面：
+
+<pre>
+initializing  B-tree ... 
++----------------------------------+
+| ip2region test script            |
+| Author: chenxin619315@gmail.com  |
+| Type 'quit' to exit program      |
++----------------------------------+
+p2region>> 101.105.35.57
+2163|中国|华南|广东省|深圳市|鹏博士 in 0.02295 millseconds
+</pre>
+
+输入ip地址开始测试，第一次会稍微有点慢，在运行命令后面接入binary来尝试binary算法，建议使用b-tree算法。
+
+具体集成请参考不同客户端的测试源码。
+
+**6. 联系作者：**
+
+狮子的魂： chenxin619315@gmail.com
---
+++ binding/c/ip2region.c
@@ -0,0 +1,343 @@
+/**
+ * default ip2region implementation
+ *
+ * @see		#ip2region.h
+ * @author	chenxin<chenxin619315@gmail.com>
+ * @date	2015-10-30
+*/
+
+#include "ip2region.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * create a new ip2region object
+ *
+ * @param	dbFile path
+*/
+IP2R_API uint_t ip2region_create(ip2region_t ip2rObj, char *dbFile)
+{
+	memset(ip2rObj, 0x00, sizeof(ip2region_entry));
+	ip2rObj->headerLen = 0;
+	ip2rObj->HeaderSip = (uint_t *) IP2R_MALLOC(TOTAL_HEADER_LENGTH);
+	if ( ip2rObj->HeaderSip == NULL )
+	{
+		return 0;
+	}
+
+	ip2rObj->HeaderPtr = (uint_t *) IP2R_MALLOC(TOTAL_HEADER_LENGTH);
+	if ( ip2rObj->HeaderPtr == NULL )
+	{
+		IP2R_FREE(ip2rObj->HeaderSip);
+		return 0;
+	}
+
+	//open the db file
+	ip2rObj->dbHandler = fopen(dbFile, "rb");
+	if ( ip2rObj->dbHandler == NULL  )
+	{
+		return 0;
+	}
+
+	ip2rObj->firstIndexPtr = 0;
+	ip2rObj->lastIndexPtr  = 0;
+	ip2rObj->totalBlocks   = 0;
+
+	return 1;
+}
+
+/**
+ * destroy the specifield ip2region object
+ *
+ * @param	ip2region_t
+*/
+IP2R_API uint_t ip2region_destroy(ip2region_t ip2rObj)
+{
+	IP2R_FREE(ip2rObj->HeaderSip);
+	ip2rObj->HeaderSip = NULL;
+	IP2R_FREE(ip2rObj->HeaderPtr);
+	ip2rObj->HeaderPtr = NULL;
+
+	//close the db file resource
+	fclose(ip2rObj->dbHandler);
+
+	return 1;
+}
+
+/**
+ * get the region associated with the specifield ip address with binary search algorithm
+ *
+ * @param	ip2rObj
+ * @param	ip
+ * @param	datablock
+ * @return	uint_t
+*/
+IP2R_API uint_t ip2region_binary_search(ip2region_t ip2rObj, uint_t ip, datablock_t datablock)
+{
+	int l, h, m, p;
+	uint_t sip, eip, dptr;
+	char buffer[256];
+	int dataLen, dataptr;
+
+	if ( ip2rObj->totalBlocks == 0 )
+	{
+		fseek(ip2rObj->dbHandler, 0, 0);
+		if ( fread(buffer, 8, 1, ip2rObj->dbHandler) != 1 ) {
+			return 0;
+		}
+
+		ip2rObj->firstIndexPtr = getUnsignedInt(buffer, 0);
+		ip2rObj->lastIndexPtr  = getUnsignedInt(buffer, 4);
+		ip2rObj->totalBlocks   = (ip2rObj->lastIndexPtr-ip2rObj->firstIndexPtr)/INDEX_BLOCK_LENGTH + 1;
+	}
+
+	//binary search the index blocks to define the data block
+	l = 0; h = ip2rObj->totalBlocks; dptr = 0;
+	while ( l <= h ) {
+		m = (l + h) >> 1;
+		p = ip2rObj->firstIndexPtr + m * INDEX_BLOCK_LENGTH;
+
+		fseek(ip2rObj->dbHandler, p, 0);
+		if ( fread(buffer, INDEX_BLOCK_LENGTH, 1, ip2rObj->dbHandler) != 1 ) {
+			return 0;
+		}
+		sip = getUnsignedInt(buffer, 0);
+		if ( ip < sip ) {
+			h = m - 1;
+		} else {
+			eip = getUnsignedInt(buffer, 4);
+			if ( ip > eip ) {
+				l = m + 1;
+			} else {
+				dptr = getUnsignedInt(buffer, 8);
+				break;
+			}
+		}
+	}
+
+	if ( dptr == 0 ) return 0;
+
+	//get the data
+	dataLen = ((dptr >> 24) & 0xFF);
+	dataptr = (dptr & 0x00FFFFFF);
+
+	//memset(data, 0x00, sizeof(data));
+	fseek(ip2rObj->dbHandler, dataptr, 0);
+	if ( fread(buffer, dataLen, 1, ip2rObj->dbHandler) != 1 ) {
+		return 0;
+	}
+
+	//fill the data to the datablock
+	datablock->city_id = getUnsignedInt(buffer, 0);
+	dataLen -= 4;	//reduce the length of the city_id
+	memcpy(datablock->region, buffer + 4, dataLen); 
+	datablock->region[dataLen] = '\0';
+
+	return 1;
+}
+
+IP2R_API uint_t ip2region_binary_search_string(ip2region_t ip2rObj, char *ip, datablock_t datablock)
+{
+	return ip2region_binary_search(ip2rObj, ip2long(ip), datablock);
+}
+
+/**
+ * get the region associated with the specifield ip address with b-tree algorithm
+ *
+ * @param	ip2rObj
+ * @param	ip
+ * @param	datablock
+ * @return	uint_t
+*/
+IP2R_API uint_t ip2region_btree_search(ip2region_t ip2rObj, uint_t ip, datablock_t datablock)
+{
+	int i, idx;
+	int l, m, h, p, sptr, eptr, indexBlockLen, dataLen, dataptr;
+	uint_t sip, eip, idxptr, dptr;
+	char buffer[TOTAL_HEADER_LENGTH];
+
+	if ( ip2rObj->headerLen == 0 )
+	{
+		idx = 0;
+		fseek(ip2rObj->dbHandler, 8, 0);	//pass the super block
+		if ( fread(buffer, TOTAL_HEADER_LENGTH, 1, ip2rObj->dbHandler) != 1 ) {
+			return 0;
+		}
+
+		for ( i = 0; i < TOTAL_HEADER_LENGTH; i += 8 )
+		{
+			sip    = getUnsignedInt(buffer, i);
+			idxptr = getUnsignedInt(buffer, i + 4);
+			if ( idxptr == 0 ) break;
+
+			ip2rObj->HeaderSip[idx] = sip;
+			ip2rObj->HeaderPtr[idx] = idxptr;
+			idx++;
+		}
+
+		ip2rObj->headerLen = idx;
+	}
+
+	//search the header block to define the index block
+	l = 0; h = ip2rObj->headerLen; sptr = 0; eptr = 0;
+	while ( l <= h ) {
+		m = ((l + h) >> 1);
+
+		//perfetc matched, just return it
+		if ( ip == ip2rObj->HeaderSip[m] ) {
+			if ( m > 0 ) {
+				sptr = ip2rObj->HeaderPtr[m-1];
+				eptr = ip2rObj->HeaderPtr[m  ];
+			} else {
+				sptr = ip2rObj->HeaderPtr[m ];
+				eptr = ip2rObj->HeaderPtr[m+1];
+			}
+
+			break;
+		}
+
+		//less then the middle value
+		if ( ip < ip2rObj->HeaderSip[m] ) {
+			if ( m == 0 ) {
+				sptr = ip2rObj->HeaderPtr[m  ];
+				eptr = ip2rObj->HeaderPtr[m+1];
+				break;
+			} else if ( ip > ip2rObj->HeaderSip[m-1] ) {
+				sptr = ip2rObj->HeaderPtr[m-1];
+				eptr = ip2rObj->HeaderPtr[m  ];
+				break;
+			}
+			h = m - 1;
+		} else {
+			if ( m == ip2rObj->headerLen - 1 ) {
+				sptr = ip2rObj->HeaderPtr[m-1];
+				eptr = ip2rObj->HeaderPtr[m  ];
+				break;
+			} else if ( ip <= ip2rObj->HeaderSip[m+1] ) {
+				sptr = ip2rObj->HeaderPtr[m  ];
+				eptr = ip2rObj->HeaderPtr[m+1];
+				break;
+			}
+			l = m + 1;
+		}
+	}
+
+	//not matched just stop it
+	if ( sptr == 0 ) return 0;
+
+	indexBlockLen = eptr - sptr;
+	fseek(ip2rObj->dbHandler, sptr, 0);
+	if ( fread(buffer, indexBlockLen + INDEX_BLOCK_LENGTH, 1, ip2rObj->dbHandler) != 1 ) {
+		return 0;
+	}
+
+	dptr = 0; l = 0; h = indexBlockLen / INDEX_BLOCK_LENGTH;
+	while ( l <= h ) {
+		m = ((l + h) >> 1);
+		p = m * INDEX_BLOCK_LENGTH;
+		sip = getUnsignedInt(buffer, p);
+		if ( ip < sip ) {
+			h = m - 1;
+		} else {
+			eip = getUnsignedInt(buffer, p + 4);
+			if ( ip > eip ) {
+				l = m + 1;
+			} else {
+				dptr = getUnsignedInt(buffer, p + 8);
+				break;
+			}
+		}
+	}
+
+	if ( dptr == 0 ) return 0;
+
+	dataLen = ((dptr >> 24) & 0xFF);
+	dataptr = (dptr & 0x00FFFFFF);
+
+	fseek(ip2rObj->dbHandler, dataptr, 0);
+	if ( fread(buffer, dataLen, 1, ip2rObj->dbHandler) != 1 ) {
+		return 0;
+	}
+
+	datablock->city_id = getUnsignedInt(buffer, 0);
+	dataLen -= 4;
+	memcpy(datablock->region, buffer + 4, dataLen);
+	datablock->region[dataLen] = '\0';
+
+	return 1;
+}
+
+IP2R_API uint_t ip2region_btree_search_string(ip2region_t ip2rObj, char *ip, datablock_t datablock)
+{
+	return ip2region_btree_search(ip2rObj, ip2long(ip), datablock);
+}
+
+/**
+ * get a unsinged long(4bytes) from a specifield buffer start from the specifield offset
+ *
+ * @param	buffer
+ * @param	offset
+ * @return	uint_t
+*/
+IP2R_API uint_t getUnsignedInt(char *buffer, int offset)
+{
+	return (
+		((buffer[offset  ]) & 0x000000FF) | 
+		((buffer[offset+1] <<  8) & 0x0000FF00) | 
+		((buffer[offset+2] << 16) & 0x00FF0000) |
+		((buffer[offset+3] << 24) & 0xFF000000)
+	);
+}
+
+/**
+ * string ip to long
+ *
+ * @param	ip
+ * @return	uint_t
+*/
+IP2R_API uint_t ip2long(char *ip)
+{
+	int i = 0, p = 24;
+	char buffer[4], *cs = ip;
+	uint_t ipval = 0;
+
+	while ( *cs != '\0' )
+	{
+		if ( *cs == '.' ) {
+			//single part length limit
+			if ( i > 3 ) {
+				ipval = 0;
+				break;
+			}
+
+			if ( p < 0 ) break;
+			buffer[i] = '\0';
+			ipval |= (atoi(buffer) << p);
+			p -= 8;
+			i = 0;
+		} else {
+			buffer[i++] = *cs;
+		}
+
+		cs++;
+	}
+
+	//append the rest parts
+	if ( i > 3 ) return 0;
+	buffer[i] = '\0';
+	ipval |= atoi(buffer);
+
+	return ipval;
+}
+
+/**
+ * long to string ip
+ *
+ * @param	ip
+ * @param	buffer
+ * @return	uint_t(1 for success and 0 for failed)
+*/
+IP2R_API uint_t long2ip(uint_t ip, char *buffer)
+{
+	return 0;
+}
