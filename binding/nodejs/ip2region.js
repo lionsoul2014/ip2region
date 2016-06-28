2e31e924b5c64686cf8639ec75eea2d6a5f8457b
dongyado <slaying.monsters@gmail.com>
2016-06-28 09:31:23 +0800

    update ip2region.js

--- binding/nodejs/ip2region.js
+++ binding/nodejs/ip2region.js
@@ -6,13 +6,16 @@
  * @author dongyado<dongyado@gmail.com>
  * */
 
+var fs   = require('fs');
 var ip2r = {};
-ip2r.db_file = null;
+ip2r.db_file    = null;
+ip2r.db_fd      = null;
 
 
 exports.setDbFile = function(path)
 {
     ip2r.db_file = path;
+    ip2r.db_fd   = fs.open(ip2r.db_file);
 }
 
 exports.getDbFile = function(path)
