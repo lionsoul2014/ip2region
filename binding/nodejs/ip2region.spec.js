c7104902e3f5a0cdf3577b491b9e53f41dc52180
leeching <leeching.fx@gmail.com>
2018-06-26 19:58:01 +0800

    refactor(nodejs): write nodejs script in javascript way

--- binding/nodejs/ip2region.spec.js
+++ binding/nodejs/ip2region.spec.js
@@ -1,15 +1,24 @@
-'use strict';
+const path = require('path');
+const IP2Region = require('./ip2region');
 
-const ip = require('./ip2region');
+describe('ip2region', () => {
+  let instance;
 
-const query = ip.create('../../data/ip2region.db');
+  beforeAll(() => {
+    instance = new IP2Region({dbPath: path.join(__dirname, '../../data/ip2region.db')});
+  });
 
-it('should query', () => {
-  expect(query.btreeSearchSync('120.24.78.68')).toMatchSnapshot();
-  expect(query.btreeSearchSync('10.10.10.10')).toMatchSnapshot();
-});
+  afterAll(() => {
+    instance.destroy();
+  });
 
-it('binarySearch', () => {
-  expect(query.binarySearchSync('120.24.78.68')).toMatchSnapshot();
-  expect(query.binarySearchSync('10.10.10.10')).toMatchSnapshot();
+  test('should query', () => {
+    expect(instance.btreeSearchSync('120.24.78.68')).toMatchSnapshot();
+    expect(instance.btreeSearchSync('10.10.10.10')).toMatchSnapshot();
+  });
+
+  test('binarySearch', () => {
+    expect(instance.binarySearchSync('120.24.78.68')).toMatchSnapshot();
+    expect(instance.binarySearchSync('10.10.10.10')).toMatchSnapshot();
+  });
 });
