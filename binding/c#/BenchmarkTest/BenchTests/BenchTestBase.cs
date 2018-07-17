33570612bc29937718de8b8404ccd63dbe47f867
MaleDong <maledong_github@outlook.com>
2018-07-17 20:43:47 +0800

    Refactor of CSharp:
    1) Add `Async` methods.
    2) Remove uesless variables, codes.
    3) Remove ConsoleApp and use UnitTest instead.
    4) Add README.md instead.

---
+++ binding/c#/BenchmarkTest/BenchTests/BenchTestBase.cs
@@ -0,0 +1,21 @@
+﻿using BenchmarkDotNet.Attributes;
+using IP2Region;
+
+namespace BenchmarkTest.BenchTests
+{
+    public class BenchTestBase
+    {
+        protected DbSearcher _dbSearcher = null;
+
+        [GlobalSetup]
+        public void Init()
+        {
+            _dbSearcher = new DbSearcher("../../../../../data/ip2region.db");
+        }
+        [GlobalCleanup]
+        public void Clearup()
+        {
+            _dbSearcher.Dispose();
+        }
+    }
+}
