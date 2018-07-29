11f527b207d004bd442c0bb80bba5b9f0db76920
RocherKong <rocher@126.com>
2018-07-30 00:31:49 +0800

    1.CodeStandardized 2.Support Netfx4.5 3.TestStandardized

---
+++ binding/c#/IP2Region.Test.Benchmark/Program.cs
@@ -0,0 +1,15 @@
+﻿using BenchmarkDotNet.Running;
+using System;
+
+namespace IP2Region.Test.Benchmark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To IP2Regin!");
+            var summary = BenchmarkRunner.Run<DbSearch_Test>();
+            Console.ReadLine();
+        }
+    }
+}
