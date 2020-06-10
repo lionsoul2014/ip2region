a9571d443fb515c81d8872d565b87df5fb32dd82
Killer <Killer@DESKTOP-QTKGU12>
2020-06-10 16:59:32 +0800

    add DataBlock add DbConfig

---
+++ maker/csharp/DbMaker/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DbMaker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
