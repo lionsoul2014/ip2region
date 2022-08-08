8199a08ee09ce799b5ae3ed8467a4a67d2f86fa6
1476166492 <lzh.sha@outlook.com>
2022-08-08 20:58:23 +0800

    csharp 2.0 xdb maker

---
+++ maker/csharp/IP2RegionMaker/XDB/IndexPolicy.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IP2RegionMaker.XDB
+{
+    public enum IndexPolicy
+    {
+        VectorIndexPolicy = 1,
+
+        BTreeIndexPolicy = 2,
+    }
+}
