72b9ffd29e6366a595b18d23a5a6dee332d9a76c
gongzhengyang <gongzhengyang@bolean.com.cn>
2022-12-20 13:55:52 +0800

    feat: add loop test ip

--- binding/rust/bin/src/cmd.rs
+++ binding/rust/bin/src/cmd.rs
@@ -4,6 +4,6 @@ pub fn get_matches() -> ArgMatches {
     Command::new("ip2region")
         .version("0.1")
         .about("ip2region bin program")
-        .arg(arg!(--xdb <xdb> "the xdb filepath, you can set this field like ../data/ip2region.xdb").required(true))
+        .arg(arg!(--xdb <xdb> "the xdb filepath, you can set this field like ../data/ip2region.xdb"))
         .get_matches()
 }
