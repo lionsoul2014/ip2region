3fef2a2fadb0284364b4c30387a730c562a62f9f
lion <chenxin619315@gmail.com>
2023-02-05 18:52:40 +0800

    fix the bug of getInt2 reported by https://github.com/lionsoul2014/ip2region/issues/287

--- binding/java/src/main/java/org/lionsoul/ip2region/xdb/Searcher.java
+++ binding/java/src/main/java/org/lionsoul/ip2region/xdb/Searcher.java
@@ -235,8 +235,8 @@ public class Searcher {
 
     public static int getInt2(byte[] b, int offset) {
         return (
-            (b[offset++] & 0x000000FF) |
-            (b[offset  ] & 0x0000FF00)
+            ((b[offset++] & 0x000000FF)) |
+            ((b[offset  ] << 8) & 0x0000FF00)
         );
     }
 
