2662fb05c27163c8d14adf903b1f66a0e0027e10
Argo Zhang <argo@live.ca>
2025-10-19 14:55:37 +0800

    feat: 增加注入服务支持

---
+++ binding/csharp/IP2Region.Net/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,27 @@
+﻿using IP2Region.Net.Abstractions;
+using IP2Region.Net.XDB;
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+/// <summary>
+/// BootstrapBlazor 服务扩展类
+/// </summary>
+public static class BootstrapBlazoIP2RegionExtensions
+{
+    /// <summary>
+    /// 添加 IP2RegionService 服务
+    /// </summary>
+    /// <param name="services"></param>
+    public static IServiceCollection AddIP2RegionService(this IServiceCollection services, string path, CachePolicy cachePolicy = CachePolicy.Content)
+    {
+        services.AddSingleton<ISearcher>( provider =>
+        {
+            return new Searcher(cachePolicy, path);
+        });
+#if NET8_0_OR_GREATER
+        services.AddKeyedSingleton<ISearcher, Searcher>("IP2Region.Net");
+#endif
+
+        return services;
+    }
+}
