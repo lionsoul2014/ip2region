9ef1fc34ac6fdb220f65eb3f4ee4070c46276e74
Argo Zhang <argo@live.ca>
2025-11-20 17:31:13 +0800

    test: 增加单元测试提高代码覆盖率

--- binding/csharp/IP2Region.Net.Test/UtilTest.cs
+++ binding/csharp/IP2Region.Net.Test/UtilTest.cs
@@ -4,11 +4,17 @@ namespace IP2Region.Net.Test;
 
 public class UtilTest
 {
-    [Theory]
-    [InlineData("114.114.114.114")]
-    public void TestIpAddressToUInt32(string value)
+    [Fact]
+    public void IpAddressToUInt32_Ok()
     {
-        var uintIp = XDB.Util.IpAddressToUInt32(value);
-        Console.WriteLine(uintIp);
+        var uintIp = XDB.Util.IpAddressToUInt32("114.114.114.114");
+        Assert.Equal((uint)1920103026, uintIp);
+    }
+
+    [Fact]
+    public void GetMidIp_Ok()
+    {
+        var uintIp = XDB.Util.GetMidIp(1, 10);
+        Assert.Equal((uint)5, uintIp);
     }
 }
