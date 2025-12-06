48dce3545efc7d0065bb74d9f9827a983c4c73b5
lionsoul2014 <chenxin619315@gmail.com>
2025-12-06 12:23:52 +0800

    parse cache policy from name

---
+++ binding/java/src/main/java/org/lionsoul/ip2region/service/InvalidCachePolicyException.java
@@ -0,0 +1,11 @@
+// Copyright 2022 The Ip2Region Authors. All rights reserved.
+// Use of this source code is governed by a Apache2.0-style
+// license that can be found in the LICENSE file.
+
+package org.lionsoul.ip2region.service;
+
+public class InvalidCachePolicyException extends Exception {
+    public InvalidCachePolicyException(String str) {
+        super(str);
+    }
+}
