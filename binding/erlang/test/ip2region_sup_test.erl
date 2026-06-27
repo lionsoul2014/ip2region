d06690f6518350d13cf9531581e6cd5b96352a88
Alice39s <alice39s@xmsl.dev>
2026-06-28 05:18:03 +0900

    test(erlang): add shared test helper and DRY app setup

--- binding/erlang/test/ip2region_sup_test.erl
+++ binding/erlang/test/ip2region_sup_test.erl
@@ -3,22 +3,6 @@
 -include("ip2region.hrl").
 
 pools_started_test() ->
-    try application:stop(ip2region) catch _:_ -> ok end,
-    try application:unload(ip2region) catch _:_ -> ok end,
-    ok = application:load(ip2region),
-    %% NOTE: rebar3 compiles tests under _build/test/lib/ip2region via a
-    %% symlink back to the source tree, so ?FILE resolves to the original
-    %% binding/erlang/test/... path. If rebar3 is configured to copy
-    %% instead of symlink, this path traversal must be adjusted.
-    TestDir = filename:dirname(?FILE),
-    ErlangDir = filename:dirname(TestDir),
-    BindingDir = filename:dirname(ErlangDir),
-    RepoRoot = filename:dirname(BindingDir),
-    V6File = filename:join([RepoRoot, "data", "ip2region_v6.xdb"]),
-    ok = application:set_env(ip2region, db, [
-        {ipv4, "ip2region.xdb"},
-        {ipv6, V6File}
-    ]),
-    {ok, _} = application:ensure_all_started(ip2region),
+    {ok, _} = ip2region_test_util:start_app(),
     ?assert(is_pid(whereis(?IP2REGION_POOL_V4))),
     ?assert(is_pid(whereis(?IP2REGION_POOL_V6))).
