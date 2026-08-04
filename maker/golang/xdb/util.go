d63df05b09406922df3cc63740109c5ef7686d46
lionsoul2014 <chenxin619315@gmail.com>
2026-08-04 09:44:08 +0800

    IPSub to IPAdd (clearer semantics)

--- maker/golang/xdb/util.go
+++ maker/golang/xdb/util.go
@@ -134,8 +134,8 @@ func IPSubOne(ip []byte) []byte {
 	return r
 }
 
-// IPSub Sub the spcecified two byte ip
-func IPSub(sip, eip []byte) ([]byte, error) {
+// IPAdd add the spcecified two byte ip
+func IPAdd(sip, eip []byte) ([]byte, error) {
 	if len(sip) != len(eip) {
 		return []byte{}, fmt.Errorf("length of the two ips are not the same")
 	}
@@ -179,7 +179,7 @@ func IPHalf(ip []byte) []byte {
 
 // IPMiddle get the middle value of two input ip address
 func IPMiddle(sip, eip []byte) ([]byte, error) {
-	buf, err := IPSub(sip, eip)
+	buf, err := IPAdd(sip, eip)
 	if err != nil {
 		return []byte{}, fmt.Errorf("IPSub(%s, %s): %w", IP2String(sip), IP2String(eip), err)
 	}
